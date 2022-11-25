afe8c8337d6406af390c00a3122c33ea983cacca
billxu <billxu0521@gmail.com>
2022-11-25 15:19:20 +0800

    pwa

--- src/main.js
+++ src/main.js
@@ -27,6 +27,7 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import './theme/custom.css';
+import './registerServiceWorker'
 
 const app = createApp(App)
   .use(IonicVue)
